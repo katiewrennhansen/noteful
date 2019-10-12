07df4ac6a8aee6fc52a78dd185ff4179c90edf3e
Katie Hansen <khansen0415@gmail.com>
2019-10-12 09:38:16 -0400

    Updates

---
+++ precache-manifest.c226d87e6a3aba74df3bad09768f8bf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c71a9a208fd0165b03a17203a4565f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1d97e1e0d0cec552f82",
+    "url": "/static/css/main.5a2fce4c.chunk.css"
+  },
+  {
+    "revision": "382738c98e08cec94653",
+    "url": "/static/js/2.b2c615f4.chunk.js"
+  },
+  {
+    "revision": "a1d97e1e0d0cec552f82",
+    "url": "/static/js/main.6e4432a1.chunk.js"
+  },
+  {
+    "revision": "0895d391c94475f57994",
+    "url": "/static/js/runtime-main.4ec025c0.js"
+  }
+]);
